bdc9bd15e58f06d4c237f1e44de1d65beacf5910
Vitor Sousa <v.sousa.cf@gmail.com>
2024-09-16 07:05:41 -0300

    fix(api): :sparkles: fix controller of refresh token

--- apps/api/src/server/functions/auth/refresh-token/handler.ts
+++ apps/api/src/server/functions/auth/refresh-token/handler.ts
@@ -1,10 +1,11 @@
+import { makeRefreshTokenController } from "@factories/controllers/auth/refresh-token";
 import { requestAdapter } from "@server/adapters/request";
 import { responseAdapter } from "@server/adapters/response";
 
 import type { APIGatewayProxyEventV2 } from "aws-lambda";
 
 export async function handler(event: APIGatewayProxyEventV2) {
-	const controller = makeController();
+	const controller = makeRefreshTokenController();
 
 	const response = await controller.handle(requestAdapter(event));
 
