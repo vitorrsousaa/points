21ea44ac9f2b4927672e903ec6a94d2640cf0cc3
Vitor Sousa <v.sousa.cf@gmail.com>
2024-09-17 06:37:12 -0300

    fix(api): :bug: fix reset password route

--- apps/api/src/server/functions/auth/reset-password/handler.ts
+++ apps/api/src/server/functions/auth/reset-password/handler.ts
@@ -1,10 +1,11 @@
+import { makeResetPasswordController } from "@factories/controllers/auth/reset-password";
 import { requestAdapter } from "@server/adapters/request";
 import { responseAdapter } from "@server/adapters/response";
 
 import type { APIGatewayProxyEventV2 } from "aws-lambda";
 
 export async function handler(event: APIGatewayProxyEventV2) {
-	const controller = makeController();
+	const controller = makeResetPasswordController();
 
 	const response = await controller.handle(requestAdapter(event));
 
