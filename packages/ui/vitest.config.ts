e876b5b9c29764ec63f7436bbef3163fb215809a
Vitor Sousa <v.sousa.cf@gmail.com>
2025-02-12 08:54:48 -0300

    perf(root): :wrench: change yarn workspace to pnpm workspace

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -1,12 +1,12 @@
-import { defineProject, mergeConfig } from "vitest/config";
 // @ts-ignore
 import configShared from "@shared/vitest-presets/browser/vitest.config";
+import { defineProject, mergeConfig } from "vitest/config";
 
 export default mergeConfig(
 	configShared,
 	defineProject({
 		test: {
-			globals: true,
+			// globals: true,
 		},
 	}),
 );
